6bd58ea52cdfcf21c2b4aa6332dc479cc4a0a092
OrEliav <oreliavzion@gmail.com>
2024-09-29 12:59:31 +0300

    Create BoardController.cs

---
+++ API/Controllers/BoardController.cs
@@ -0,0 +1,31 @@
+﻿using KanBan_2024.ServiceLayer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BoardController : Controller
+    {
+        private ServiceFactory _serviceFactory;
+
+        public BoardController(ServiceFactory serviceFactory)
+        {
+            _serviceFactory = serviceFactory;
+        }
+
+        //POST: BoardController/create
+        [HttpPost("create")]
+        public ActionResult Create([FromBody] BoardCreate boardCreate)
+        {
+            var response = _serviceFactory.BS.CreateBoard(boardCreate.Email, boardCreate.boardName);
+            return Content(response, "application/json");
+        }
+    }
+    public class BoardCreate
+    {
+        public string Email { get; set; }
+        public string boardName { get; set; }
+    }
+
+}
