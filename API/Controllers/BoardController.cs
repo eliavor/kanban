b1212181d9741e1a3f013a714315ef94523aad0a
eliavor <eliavor@post.bgu.ac.il>
2024-09-29 13:14:46 +0300

    board+task controllers

--- API/Controllers/BoardController.cs
+++ API/Controllers/BoardController.cs
@@ -1,5 +1,6 @@
 ﻿using KanBan_2024.ServiceLayer;
 using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace API.Controllers
 {
@@ -7,25 +8,166 @@ namespace API.Controllers
     [Route("[controller]")]
     public class BoardController : Controller
     {
-        private ServiceFactory _serviceFactory;
+        private readonly ServiceFactory _serviceFactory;
 
         public BoardController(ServiceFactory serviceFactory)
         {
             _serviceFactory = serviceFactory;
         }
 
-        //POST: BoardController/create
+        // POST: BoardController/create
         [HttpPost("create")]
         public ActionResult Create([FromBody] BoardCreate boardCreate)
         {
-            var response = _serviceFactory.BS.CreateBoard(boardCreate.Email, boardCreate.boardName);
+            var response = _serviceFactory.BS.CreateBoard(boardCreate.Email, boardCreate.BoardName);
+            return Content(response, "application/json");
+        }
+
+        // DELETE: BoardController/delete
+        [HttpDelete("delete")]
+        public ActionResult Delete([FromBody] BoardDelete boardDelete)
+        {
+            var response = _serviceFactory.BS.DeleteBoard(boardDelete.Email, boardDelete.BoardName);
+            return Content(response, "application/json");
+        }
+
+        // POST: BoardController/limitColumn
+        [HttpPost("limitColumn")]
+        public ActionResult LimitColumn([FromBody] LimitColumnRequest request)
+        {
+            var response = _serviceFactory.BS.LimitColumn(request.Email, request.BoardName, request.ColumnOrdinal, request.Limit);
+            return Content(response, "application/json");
+        }
+
+        // POST: BoardController/addTask
+        [HttpPost("addTask")]
+        public ActionResult AddTask([FromBody] AddTaskRequest request)
+        {
+            var response = _serviceFactory.BS.AddTask(request.Email, request.BoardName, request.Title, request.Description, request.DueDate);
+            return Content(response, "application/json");
+        }
+
+        // POST: BoardController/advanceTask
+        [HttpPost("advanceTask")]
+        public ActionResult AdvanceTask([FromBody] AdvanceTaskRequest request)
+        {
+            var response = _serviceFactory.BS.AdvanceTask(request.Email, request.BoardName, request.ColumnOrdinal, request.TaskId);
+            return Content(response, "application/json");
+        }
+
+        // GET: BoardController/getColumnLimit
+        [HttpGet("getColumnLimit")]
+        public ActionResult GetColumnLimit([FromQuery] string email, [FromQuery] string boardName, [FromQuery] int columnOrdinal)
+        {
+            var response = _serviceFactory.BS.GetColumnLimit(email, boardName, columnOrdinal);
+            return Content(response, "application/json");
+        }
+
+        // GET: BoardController/getColumnName
+        [HttpGet("getColumnName")]
+        public ActionResult GetColumnName([FromQuery] string email, [FromQuery] string boardName, [FromQuery] int columnOrdinal)
+        {
+            var response = _serviceFactory.BS.GetColumnName(email, boardName, columnOrdinal);
+            return Content(response, "application/json");
+        }
+
+        // GET: BoardController/getColumn
+        [HttpGet("getColumn")]
+        public ActionResult GetColumn([FromQuery] string email, [FromQuery] string boardName, [FromQuery] int columnOrdinal)
+        {
+            var response = _serviceFactory.BS.GetColumn(email, boardName, columnOrdinal);
+            return Content(response, "application/json");
+        }
+
+
+
+        // GET: BoardController/getBoardName
+        [HttpGet("getBoardName")]
+        public ActionResult GetBoardName([FromQuery] int boardId)
+        {
+            var response = _serviceFactory.BS.GetBoardName(boardId);
+            return Content(response, "application/json");
+        }
+
+        // POST: BoardController/transferOwnership
+        [HttpPost("transferOwnership")]
+        public ActionResult TransferOwnership([FromBody] TransferOwnershipRequest request)
+        {
+            var response = _serviceFactory.BS.TransferOwnership(request.CurrentOwnerEmail, request.NewOwnerEmail, request.BoardName);
+            return Content(response, "application/json");
+        }
+
+        // POST: BoardController/joinBoard
+        [HttpPost("joinBoard")]
+        public ActionResult JoinBoard([FromBody] JoinBoardRequest request)
+        {
+            var response = _serviceFactory.BS.JoinBoard(request.Email, request.BoardID);
+            return Content(response, "application/json");
+        }
+
+        // POST: BoardController/leaveBoard
+        [HttpPost("leaveBoard")]
+        public ActionResult LeaveBoard([FromBody] LeaveBoardRequest request)
+        {
+            var response = _serviceFactory.BS.LeaveBoard(request.Email, request.BoardID);
             return Content(response, "application/json");
         }
     }
+
+    // DTO classes for requests
     public class BoardCreate
     {
         public string Email { get; set; }
-        public string boardName { get; set; }
+        public string BoardName { get; set; }
     }
 
+    public class BoardDelete
+    {
+        public string Email { get; set; }
+        public string BoardName { get; set; }
+    }
+
+    public class LimitColumnRequest
+    {
+        public string Email { get; set; }
+        public string BoardName { get; set; }
+        public int ColumnOrdinal { get; set; }
+        public int Limit { get; set; }
+    }
+
+    public class AddTaskRequest
+    {
+        public string Email { get; set; }
+        public string BoardName { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+    }
+
+    public class AdvanceTaskRequest
+    {
+        public string Email { get; set; }
+        public string BoardName { get; set; }
+        public int ColumnOrdinal { get; set; }
+        public int TaskId { get; set; }
+    }
+
+    public class TransferOwnershipRequest
+    {
+        public string CurrentOwnerEmail { get; set; }
+        public string NewOwnerEmail { get; set; }
+        public string BoardName { get; set; }
+    }
+
+    public class JoinBoardRequest
+    {
+        public string Email { get; set; }
+        public int BoardID { get; set; }
+    }
+
+    public class LeaveBoardRequest
+    {
+        public string Email { get; set; }
+        public int BoardID { get; set; }
+    }
 }
---
+++ API/Controllers/TaskController.cs
@@ -0,0 +1,54 @@
+﻿using KanBan_2024.ServiceLayer;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TaskController : ControllerBase
+    {
+        private readonly ServiceFactory _serviceFactory;
+
+        public TaskController(ServiceFactory serviceFactory)
+        {
+            _serviceFactory = serviceFactory;
+        }
+
+        [HttpPut("update-due-date")]
+        public IActionResult UpdateTaskDueDate(string email, string boardName, int columnOrdinal, int taskId, DateTime dueDate)
+        {
+            var response = _serviceFactory.TS.UpdateTaskDueDate(email, boardName, columnOrdinal, taskId, dueDate);
+            return Content(response);
+        }
+
+        [HttpPut("update-description")]
+        public IActionResult UpdateTaskDescription(string email, string boardName, int columnOrdinal, int taskId, string description)
+        {
+            var response = _serviceFactory.TS.UpdateTaskDescription(email, boardName, columnOrdinal, taskId, description);
+            return Content(response);
+        }
+
+        [HttpPut("update-title")]
+        public IActionResult UpdateTaskTitle(string email, string boardName, int columnOrdinal, int taskId, string title)
+        {
+            var response = _serviceFactory.TS.UpdateTaskTitle(email, boardName, columnOrdinal, taskId, title);
+            return Content(response);
+        }
+
+        [HttpGet("in-progress-tasks")]
+        public IActionResult InProgressTasks(string email)
+        {
+            var response = _serviceFactory.TS.InProgressTasks(email);
+            return Content(response);
+        }
+
+        [HttpPut("assign-task")]
+        public IActionResult AssignTask(string email, string boardName, int columnOrdinal, int taskID, string emailAssignee)
+        {
+
+            var response = _serviceFactory.TS.AssignTask(email, boardName, columnOrdinal, taskID, emailAssignee);
+            return Content(response);
+        }
+    }
+}
