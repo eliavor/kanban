181a78fc987ebb8ea8bd535812730445882daf3f
eliavor <eliavor@post.bgu.ac.il>
2024-09-29 12:23:46 +0300

    user service api

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,59 @@
+﻿using KanBan_2024.ServiceLayer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : Controller
+    {
+        private ServiceFactory _serviceFactory;
+
+        public UserController(ServiceFactory serviceFactory)
+        {
+            _serviceFactory = serviceFactory;
+        }
+
+        // POST: UserController/Register
+        [HttpPost("register")]
+        public ActionResult Register([FromBody] UserRegistrationDto registrationDto)
+        {
+            var response = _serviceFactory.US.Register(registrationDto.Email, registrationDto.Password);
+            return Content(response, "application/json");
+        }
+
+        // POST: UserController/Login
+        [HttpPost("login")]
+        public ActionResult Login([FromBody] UserLoginDto loginDto)
+        {
+            var response = _serviceFactory.US.Login(loginDto.Email, loginDto.Password);
+            return Content(response, "application/json");
+        }
+
+        // POST: UserController/Logout
+        [HttpPost("logout")]
+        public ActionResult Logout([FromBody] UserLogoutDto logoutDto)
+        {
+            var response = _serviceFactory.US.Logout(logoutDto.Email);
+            return Content(response, "application/json");
+        }
+    }
+
+    public class UserRegistrationDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class UserLoginDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class UserLogoutDto
+    {
+        public string Email { get; set; }
+    }
+}
---
+++ API/Program.cs
@@ -0,0 +1,29 @@
+using KanBan_2024.ServiceLayer;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddSingleton<ServiceFactory>();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+//app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
