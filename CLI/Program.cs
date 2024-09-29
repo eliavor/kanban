d2961a936ae8bb17cab8befd7940cd1a8896d5ba
eliavor <eliavor@post.bgu.ac.il>
2024-09-29 09:01:09 +0300

    CLI Start

---
+++ CLI/Program.cs
@@ -0,0 +1,39 @@
+﻿using KanBan_2024.ServiceLayer;
+
+namespace CLI
+{
+    class CLI
+    {
+        public static void Main(string[] args)
+        {
+            ServiceFactory SF = new ServiceFactory();
+
+            while (true)
+            {
+
+                Console.WriteLine("1. Register \n2. Login \n3. Create Board \n4. Add task ");
+                string input = Console.ReadLine();
+                if (input == null || input.Length > 1)
+                {
+                    throw new Exception("Illegal Argument Exception");
+                }
+                switch (input)
+                {
+                    case "1":
+                        Console.WriteLine("Enter Email: ");
+                        string email = Console.ReadLine();
+                        Console.WriteLine("Enter Password: ");
+                        string password = Console.ReadLine();
+
+                        Console.WriteLine(SF.US.Register(email, password));
+
+                        break;
+
+                    case "2":
+                        Console.WriteLine("2");
+                        break;
+                }
+            }
+        }
+    }
+}
